5ba222e10171f74e65f61eeb7c7a67468054858f
Danny Cao <dannycao1997@yahoo.com>
2024-03-31 23:01:59 -0400

    working on integer duplicate deleter

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -5,4 +5,20 @@ package com.zipcodewilmington.looplabs;
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
+    public IntegerDuplicateDeleter(Integer[] intArray) {
+        super(intArray);
+    }
+
+    @Override  //removing integers that appear at least a specified number of times
+    public Integer[] removeDuplicates(int maxNumberOfDuplications) {
+        return removeBasedOnResultCount(maxNumberOfDuplications, false);
+    }
+
+    @Override //removing integers that appear exactly a specified number of times
+    public Integer[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        return removeBasedOnResultCount(exactNumberOfDuplications, true);
+    }
 }
+
+
+
